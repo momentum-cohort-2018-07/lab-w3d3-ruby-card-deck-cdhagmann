9927c23f310d8effe65ddda2e9e2e82e1168b82c
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-08-15 13:43:15 -0400

    Initial Commit

--- deck_starter_files/lib/card.rb
+++ deck_starter_files/lib/card.rb
@@ -1,2 +1,21 @@
 class Card
+
+  attr_reader :rank
+  attr_reader :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def greater_than? (card)
+    order = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+    order.index(card.rank) < order.index(@rank)
+   end 
+
+  def ==(another_card)
+    self.suit == another_card.suit &&
+    self.rank == another_card.rank
+  end
+
 end
--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -1,4 +1,30 @@
 require_relative "card"
 
 class Deck
+
+  def initialize
+    @deck =  []
+    [:clubs, :diamonds, :hearts, :spades].reverse.each do |suit|
+      [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K].reverse.each do |rank |
+        @deck << Card.new(rank, suit)
+      end
+    end
+  end
+
+  def deck
+    @deck
+  end  
+  def cards_left
+    @deck.length
+  end
+
+  def draw
+    @deck.pop
+  end
+
+  def shuffle
+    @deck.shuffle
+  end
+
+
 end
