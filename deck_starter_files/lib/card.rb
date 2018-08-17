ae93afd96b074439e71dffd21dbd84d0810e921c
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-08-17 11:42:56 -0400

    Version 1. Working version

--- deck_starter_files/lib/card.rb
+++ deck_starter_files/lib/card.rb
@@ -9,11 +9,11 @@ class Card
 
   def greater_than?(card)
     order = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
-    order.index(@rank) > order.index(card.rank) 
-  end 
+    order.index(@rank) > order.index(card.rank)
+  end
 
   def ==(another_card)
     self.suit == another_card.suit &&
-    self.rank == another_card.rank
+        self.rank == another_card.rank
   end
 end
